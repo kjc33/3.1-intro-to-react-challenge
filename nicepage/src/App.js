6cc20456fc8f802ef765e6f392bcc66a803ce684
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2023-11-14 23:16:59 -0800

    Init Commit

---
+++ nicepage/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Business and Industrial Parks</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ nicepage/src/components/NicePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './App.css'
+
+function NicePage() {
+  return <div>NicePage</div>;
+}
+
+export default NicePage;
