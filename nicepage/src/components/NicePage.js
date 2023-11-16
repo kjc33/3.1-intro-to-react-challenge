6547677051601e3284834c8afc3a851b472a1365
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2023-11-15 23:53:07 -0800

    Added About Section

--- nicepage/src/components/NicePage.js
+++ nicepage/src/components/NicePage.js
@@ -1,10 +1,9 @@
 import React from "react";
-import hero from "../assets/BusinessBuilding.jpeg";
-// import HeroHeadshot from "../assets/HeroHeadshot.jpeg";
+import hero from "../assets/business-building.jpeg";
 
 function NicePage() {
   return (
-    <>
+    <div className="page-wrapper">
       <section className="page-name">
         <div className="inner-container max-width flex flex-column flex-justify-center flex-align-center">
           <div className="main-heading">
@@ -30,7 +29,22 @@ function NicePage() {
           </div>
         </div>
       </section>
-    </>
+      <section className="about">
+        <div className="inner-container max-width flex flex-column">
+          <div className="image-right-content-left flex flex-column flex-mobile-row-641">
+            <div className="content flex flex-column">
+              <h2 className="about-heading">Construction Machinery Co.</h2>
+              <h3 className="subhead">We have a proud tradition of service as a General Contractor since 1974</h3>
+              <p>We headquartered in San Diego, brings over 42 years of local and national construction experience; with a reputation for delivering large and unique design-build projects ahead of schedule and on budget.  Our company has served clients and building owners for over four decades.</p>
+              <a className="btn cta-btn" href="https://www.google.com" target="_blank" rel="noreferrer">
+              Read More
+            </a>
+            </div>
+            <div className="image"></div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
 
